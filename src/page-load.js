4fa1309c46a1388399bad8cf99d55cd2da02157b
Jamie Oppel <jimmyoppelpersonal@gmail.com>
2021-01-31 19:57:06 -0800

    Added navbar at top and changed some styling

--- src/page-load.js
+++ src/page-load.js
@@ -3,31 +3,40 @@ function load_image() {
     //create image element and add attributes
     let img = document.createElement('img')
     img.src = '../media/kawaii_toast.png'
-    img.alt = 'cute toast'
+    img.alt = 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/2-cute-kawaii-toast-and-toaster-valentina-hramov.jpg'
+    img.id = 'cute_toast'
 
-    //append img to node
+    //grab div#content
     let content = document.getElementById('content')
+
+    //append link/para to div
     content.appendChild(img)
-
-    //create paragraph and link for image source
-    let p = document.createElement('p')
-    let a = document.createElement('a')
-    p.textContent = 'For image source click '
-    p.id = 'img_credit'
-    a.textContent = 'here'
-    a.href = 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/2-cute-kawaii-toast-and-toaster-valentina-hramov.jpg'
-    a.target = '_blank'
-
-    //append link to paragraph, then paragraph to div
-    p.appendChild(a)
-    content.appendChild(p)
 }
 
 function load_text() {
     //create 
 }
 
+function load_header() {
+
+    //create unordered list and tabs then append
+    let ul = document.createElement('ul')
+    let tab1 = document.createElement('li')
+    let tab2 = document.createElement('li')
+    let tab3 = document.createElement('li')
+    tab1.textContent = 'Tab 1'
+    tab1.style.float = 'left'
+    tab2.textContent = 'Tab 2'
+    tab2.style.float = 'center'
+    tab3.textContent = 'Tab 3'
+    tab3.style.float = 'right'
+    ul.appendChild(tab1); ul.appendChild(tab2); ul.appendChild(tab3);
+    let content = document.getElementById('content')
+    content.appendChild(ul)
+}
+
 function page_load() {
+    load_header()
     load_image()
 }
 
