919b988cb1bfa06c61954fa352771c36faf82c46
Jamie Oppel <jimmyoppelpersonal@gmail.com>
2021-02-02 19:42:58 -0800

    About tab is now highlighted when on the tab

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import {load_about_page} from './about_page.js'
 import {load_header} from './header_nav.js'
 
 function clear_page() {
-    let current_tab = document.querySelector('.nav_tab')
-    current_tab.remove()
+    let current_page = document.querySelector('.nav_tab')
+    current_page.remove()
+    current_tab.style.backgroundColor = 'lightskyblue'
 }
 
 //Grab div#content
@@ -12,10 +13,13 @@ let content = document.querySelector('div#content')
 //Start on about page
 content.appendChild(load_header())
 content.appendChild(load_about_page())
+let current_tab = document.getElementById('about_tab')
 
 //Attach function to 'about us' tab
 let about_tab = document.getElementById('about_tab')
-about_tab.onclick = function() {
+about_tab.onclick = function(current_tab) {
     clear_page()
     content.appendChild(load_about_page())
+    current_tab = document.getElementById('about_tab')
+    current_tab.style.backgroundColor = 'darkcyan'
 }
